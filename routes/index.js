ca7c480d4290a92f5883e9517f4ca278bbb022a2
Powell, Anthony <powell.anthony.e@gmail.com>
2016-12-26 08:20:52 -0600

    Rejected commit: Default

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dynamic Markets Analysis' });
+router.get('/', function (req, res, next) {
+		res.render('index', {title: 'Dynamic Markets Analysis'});
 });
 
 module.exports = router;
